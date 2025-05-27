7078f9ff2c5e7be2f79b32acf0820b744ecf7e55
xalonious <xander.lauwens@student.hogent.be>
2025-05-27 21:51:37 +0200

    Fix ping command

--- src/commands/info/ping.js
+++ src/commands/info/ping.js
@@ -1,15 +1,16 @@
-const { EmbedBuilder } = require("discord.js")
+const { EmbedBuilder } = require("discord.js");
+
 module.exports = {
     name: "ping",
     description: "Returns bot latency",
 
-    run: async(client, interaction) => {
+    run: async (client, interaction) => {
         await interaction.deferReply();
 
-        const pinging = await interaction.editReply({ content: "🏓 Pinging..."})
+        const pinging = await interaction.editReply({ content: "🏓 Pinging..." });
 
         const wsPing = client.ws.ping;
-        const apiPing = Date.now() - pinging.createdTimestamp;
+        const apiPing = pinging.createdTimestamp - interaction.createdTimestamp;
 
         let days = Math.floor(client.uptime / 86400000);
         let hours = Math.floor(client.uptime / 3600000) % 24;
@@ -17,10 +18,18 @@ module.exports = {
         let seconds = Math.floor(client.uptime / 1000) % 60;
 
         const pingEmbed = new EmbedBuilder()
-        .setTitle("🏓 | Pong!")
-        .setDescription(`**Websocket:** \`${wsPing}ms\`\n**API:** \`${apiPing}ms\`\n**Uptime:** \`${days}d ${hours}h ${minutes}m ${seconds}s\``)
-        .setColor("Blurple")
-        .setFooter({text: `${client.user.username}`, iconURL: client.user.displayAvatarURL()})
-        await interaction.editReply({ content: "", embeds: [pingEmbed]})
+            .setTitle("🏓 | Pong!")
+            .setDescription(
+                `**Websocket:** \`${wsPing}ms\`\n` +
+                `**API:** \`${apiPing}ms\`\n` +
+                `**Uptime:** \`${days}d ${hours}h ${minutes}m ${seconds}s\``
+            )
+            .setColor("Blurple")
+            .setFooter({
+                text: `${client.user.username}`,
+                iconURL: client.user.displayAvatarURL()
+            });
+
+        await interaction.editReply({ content: "", embeds: [pingEmbed] });
     }
-}
+};
